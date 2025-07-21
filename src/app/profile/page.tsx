8b7806a2083b4b07e5d5b6abd19f4a80b226edd4
arturkarapetyan-1981 <arturkarapetyanforwork@gmail.com>
2025-07-21 12:24:38 +0400

    I added Purchase component in profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from 'react';
 import BecomePartner from '@/components/partner/BecomePartner';
-
+import PackagePurchase from '@/components/PackagePurchase'
 export default function Profile() {
   return (
-    <Suspense fallback={<div>Loading blog...</div>}>
+    <Suspense fallback={<div className="text-white">Loading blog...</div>}>
       <BecomePartner />
+      <PackagePurchase />
     </Suspense>
   );
 }
